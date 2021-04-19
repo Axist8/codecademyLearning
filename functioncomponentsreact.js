efac0ba901b9f111646af70a25d4af6d4234ffa0
axel_j <axist8@gmail.com>
2021-04-18 21:38:14 -0700

    selectors

--- functioncomponentsreact.js
+++ functioncomponentsreact.js
@@ -329,3 +329,48 @@ export default function PageTitle() {
   );
 }
 
+  // implementation of selectors in slice components (allRecipesSlice)
+  import allRecipesData from '../../../data.js'
+  import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
+  
+  export const loadData = () => {
+    return {
+      type: 'allRecipes/loadData',
+      payload: allRecipesData
+    }
+  }
+  
+  const initialState = [];
+  export const allRecipesReducer = (allRecipes = initialState, action) => {
+    switch (action.type) {
+      case 'allRecipes/loadData':
+        return action.payload;
+      case 'favoriteRecipes/addRecipe':
+        return allRecipes.filter(recipe => recipe.id !== action.payload.id);
+      case 'favoriteRecipes/removeRecipe':
+        return [...allRecipes, action.payload]
+      default:
+        return allRecipes;
+    }
+  }
+  
+  // Implement the selectors below.
+  export const selectAllRecipes = state => state.allRecipes;
+  export const selectFilteredAllRecipes = state => {
+    const allRecipes = selectAllRecipes(state);
+    const searchTerm = selectSearchTerm(state);
+    return allRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+  
+   // useSelector
+  // Todos.js
+  import { useSelector } from 'react-redux';
+  import { selectTodos } from 'todosSlice.js';
+   
+  export const Todos = () => {
+    const todos = useSelector(selectTodos);
+   
+    return (
+      <p>{todos}</p>
+    )
+  };
